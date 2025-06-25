92d54e16986f5a35e668d2456bfd846a71c2e48b
Alan Tong <tongminglun@msn.com>
2025-06-25 23:53:43 +0800

    bit by bit

--- action/generate_index.py
+++ action/generate_index.py
@@ -2,16 +2,19 @@ import os
 
 def make_index(directory, rel_path=""):
     files = []
+    dirs = []
     for fname in sorted(os.listdir(directory)):
         fpath = os.path.join(directory, fname)
         if os.path.isfile(fpath):
             files.append(f'<li><a href="{rel_path}{fname}">{fname}</a></li>')
         elif os.path.isdir(fpath):
-            files.append(f'<li><a href="{rel_path}{fname}/index.html">{fname}/</a></li>')
+            dirs.append(f'<li><a href="{rel_path}{fname}/index.html">{fname}/</a></li>')
             make_index(fpath, rel_path + fname + "/")
-    if files:
-        with open(os.path.join(directory, "index.html"), "w", encoding="utf-8") as f:
-            f.write("<ul>\n" + "\n".join(files) + "\n</ul>")
+    with open(os.path.join(directory, "index.html"), "w", encoding="utf-8") as f:
+        if dirs:
+            f.write("<ul>\n" + "\n".join(dirs) + "\n")
+        if files:    
+            f.write("\n".join(files) + "\n</ul>")
 
 make_index("FGDB")
 make_index("FGDB/BUS")
