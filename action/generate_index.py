33666b6ff5e7e69d52e89d3d37b2f18c5afdf36a
Alan Tong <tongminglun@msn.com>
2025-06-30 21:51:49 +0800

    update

--- action/generate_index.py
+++ action/generate_index.py
@@ -11,9 +11,10 @@ def make_index(directory, rel_path=""):
             dirs.append(f'<li><a href="{rel_path}{fname}/index.html">{fname}/</a></li>')
             make_index(fpath)
     with open(os.path.join(directory, "index.html"), "w", encoding="utf-8") as f:
+        f.write("<html><style>ul { font-size: 40px; font-family: Helvetica; }</style>")
         if dirs:
             f.write("<ul>\n" + "\n".join(dirs) + "\n")
         if files:    
             f.write("\n".join(files) + "\n</ul>")
-
+        f.write("</html>")
 make_index("FGDB")
